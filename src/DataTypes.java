da44499c4446b227c0d570b7d99a0a23d8ee4d26
liuambe2 <ambersj.liu@mail.utoronto.ca>
2024-09-16 15:55:25 -0400

    finished task 1

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class DataTypes {
     // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(List<Integer> numbers) {
 
-        int s = 0;
+        long s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
         }
         return s;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println(sum(numbers));
     }
 }
